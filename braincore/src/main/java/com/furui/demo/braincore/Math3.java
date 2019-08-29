80fb0099402b0727f94898d2b1593f2062f3518f
furui <furui@qunhemail.com>
2019-08-29 17:55:58 +0800

    init a maven project

---
+++ braincore/src/main/java/com/furui/demo/braincore/Math3.java
@@ -0,0 +1,23 @@
+/*
+ * Math3.java
+ * Copyright 2019 Qunhe Tech, all rights reserved.
+ * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
+ */
+
+package com.furui.demo.braincore;
+
+/**
+ * @author furui
+ *
+ */
+public class Math3 {
+    /**
+     * addition of two doubles
+     * @param a
+     * @param b
+     * @return
+     */
+    public static double add(final double a, final double b) {
+        return a + b;
+    }
+}

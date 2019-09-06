b179e28b3ccec0fa2fb964325a94cf08eb5e623a
furui <furui@qunhemail.com>
2019-09-06 18:18:02 +0800

    add the consumer app

---
+++ mavenDemo/src/main/java/com/furui/maven/App.java
@@ -0,0 +1,16 @@
+package com.furui.maven;
+
+import com.furui.demo.braincore.Math3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+
+        final double sum = Math3.add(3.0, 4.5);
+        System.out.println(sum);
+    }
+}

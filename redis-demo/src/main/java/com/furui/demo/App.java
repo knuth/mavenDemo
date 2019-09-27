589d2ad26705f5de2cb0b0214cce6b8f9c47a863
furui <furui@qunhemail.com>
2019-09-27 14:34:13 +0800

    redis test

--- redis-demo/src/main/java/com/furui/demo/App.java
+++ redis-demo/src/main/java/com/furui/demo/App.java
@@ -1,13 +1,42 @@
 package com.furui.demo;
 
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(final String[] args) {
+        System.out.println("Redis demo>>>");
+
+        final Jedis jedis = new Jedis();
+        jedis.set("events/city", "32,15,223,828");
+        final String cachedValue = jedis.get("events/city");
+
+        System.out.println(cachedValue);
+
+        jedis.lpush("queue", "first");
+        jedis.lpush("queue", "second");
+
+        final String task = jedis.rpop("queue");
+        System.out.println(task);
+
+        //Sets
+        jedis.sadd("nicknames", "jerry");
+        jedis.sadd("nicknames", "tom");
+        final Set<String> names = jedis.smembers("nicknames");
+        final boolean exists = jedis.sismember("nicknames", "jerry");
+
+        //hashes
+        jedis.hset("user#1", "name", "peter");
+        jedis.hset("user#1", "job", "politician");
+
+        final String name = jedis.hget("user#1", "name");
+        System.out.println(name);
+
+        //transaction
     }
 }
